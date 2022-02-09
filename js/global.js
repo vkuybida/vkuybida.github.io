a8a60c764e0c8ed5fa1d0816116249ea5b8afbea
Volodymyr Kuybida <vkuybida@gmail.com>
2022-02-09 14:50:05 +0200

    update

--- js/global.js
+++ js/global.js
@@ -1,4 +1,23 @@
 $(document).ready(function () {
+/* Sticky menu */
+    window.onscroll = function() {myFunction()};
+        var navbar = document.getElementById("header");
+        var sticky = navbar.offsetTop;
+    function myFunction() {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky")
+        } else {
+            navbar.classList.remove("sticky");
+        }
+    }
+
+/* Search placeholder change on mobile */
+    if ($(window).width() < 1060 ) {
+        $(".search-holder input").attr("placeholder","Search McCain");
+    }
+    else { $(".search-holder input").attr("placeholder","Type to search here...");}
+
+
 /* Search */
     $('header .top .search-icon').click(function () {
         $('header').toggleClass('active'); return false;
@@ -12,6 +31,7 @@ $(document).ready(function () {
 /* mobile-menu */
     $('.mobile-menu').click(function () {
         $(this).toggleClass('active');
+        $('#header').toggleClass('active');
         $('header .top').toggleClass('active');
         $('header .navigation-holder').toggleClass('active');  return false;
     });
@@ -23,4 +43,14 @@ $(document).ready(function () {
     $('.faq-item h3').click(function () {
         $(this).parent('.faq-item').toggleClass('active'); return false;
     });
+
+/* Menu subitems */
+    if ($(window).width() < 1060) {
+        $('.menu-item.subitems').click(function () {
+            $(this).toggleClass('active'); return false;
+        });
+    }
+    else {
+        return false;
+    }
 });
