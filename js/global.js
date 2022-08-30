a0c6b97af6262b16d587f924fbc62aa350e98ef1
Volodymyr Kuybida <vkuybida@gmail.com>
2022-08-30 14:16:05 +0200

    update CEU FS

--- js/global.js
+++ js/global.js
@@ -2,4 +2,13 @@ $(function(){
 	$("#show-filters").on('click', function() {
 		$('.products-wrapper').toggleClass('active')
 	})
+
+	$(".menu-open").click(function () {
+		$(".mobile-menu-section").css("display", "block");
+	});
+
+	$(".menu-close").click(function () {
+		$(".mobile-menu-section").css("display", "none");
+	});
+
 })
